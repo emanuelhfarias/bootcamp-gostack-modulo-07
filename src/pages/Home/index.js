452ad1daa1e32dfcaa102db51fedecc7629d5534
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-01 17:39:35 -0300

    consume Products from API
    format money using native JS functions (Intl.NumberFormat)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,112 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapato-social-gofer-verniz-masculino/16/J43-0176-016/J43-0176-016_zoom1.jpg?ims=326x"
-          alt="Sapato"
-        />
-        <strong>Sapato cawboi: calçado com um texto muito muito longo</strong>
-        <span>R$ 129,90</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapato-social-gofer-verniz-masculino/16/J43-0176-016/J43-0176-016_zoom1.jpg?ims=326x"
-          alt="Sapato"
-        />
-        <strong>Sapato cawboi</strong>
-        <span>R$ 129,90</span>
+    this.setState({ products: data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapato-social-gofer-verniz-masculino/16/J43-0176-016/J43-0176-016_zoom1.jpg?ims=326x"
-          alt="Sapato"
-        />
-        <strong>Sapato cawboi</strong>
-        <span>R$ 129,90</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapato-social-gofer-verniz-masculino/16/J43-0176-016/J43-0176-016_zoom1.jpg?ims=326x"
-          alt="Sapato"
-        />
-        <strong>Sapato cawboi</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapato-social-gofer-verniz-masculino/16/J43-0176-016/J43-0176-016_zoom1.jpg?ims=326x"
-          alt="Sapato"
-        />
-        <strong>Sapato cawboi</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapato-social-gofer-verniz-masculino/16/J43-0176-016/J43-0176-016_zoom1.jpg?ims=326x"
-          alt="Sapato"
-        />
-        <strong>Sapato cawboi</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
